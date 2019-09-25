108c9e44b7ad1312da142512b66eac85ff391a62
Shubham Kumar <writetoshubham@gmail.com>
2019-09-25 16:04:20 +0530

    updated runtime setting

--- src/main/java/com/learn/session/WebApplication.java
+++ src/main/java/com/learn/session/WebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
+	
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(WebApplication.class);
